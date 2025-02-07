4244a24e32a7c5a12e0ca36aa792396dd8e6e178
JorgeAChacon <62924612+JorgeAChaconL@users.noreply.github.com>
2025-02-06 19:34:41 -0600

    Add two methods, first retrieve basic info, second select a substitude teacher

--- main.js
+++ main.js
@@ -24,4 +24,17 @@ class School{
             console.log(`Invalid Input | numberOfStudents: ${val} must be set to a Number`);
         }
     }
+
+    quickFacts(){
+        console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`);
+    }
+
+    static pickSubstituteTeacher(substitudeTeacher){
+        substitudeTeacher = ['Teacher1', 'Teacher2', 'Teacher3', 'Teacher4', 'Teacher5'];
+        let randomTeacherSelect = Math.floor(Math.random() * substitudeTeacher.length);
+        console.log(substitudeTeacher[randomTeacherSelect]);
+        return substitudeTeacher[randomTeacherSelect];
+    }
 }
+
+
